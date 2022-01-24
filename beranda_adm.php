c22b307e7604014fd4f69422493f1acb10cb92b0
windaputri06 <98294235+windaputri06@users.noreply.github.com>
2022-01-24 09:14:10 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,41 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Penggajian Karyawan PTPN1</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                       <p align="center"><img src="img/ptpTn-1.jpg"></img></p>
+                          <p class="label-title" align="center"><strong>PTPN1</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Penggajian Karyawan PTPN1.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data Karyawan,  
+                            dan juga pengelolaan laporan data karyawan serta absensi dan gaji karyawan.<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data karyawan,absensi dan data gaji karyawan.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
